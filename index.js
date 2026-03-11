81abac698a41c05359f028b803e095c2f616a406
Viveka31 <viveka310820@gmail.com>
2026-03-11 19:15:17 +0530

    fixed mongoose connection

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const app = express();
 app.use(express.json()); // To parse JSON bodies
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
-.catch(err => console.log("MongoDB connection error:", err));
+.catch((err) => console.log("MongoDB connection error:", err));
+
 
 // Routes
 app.use("/api/users", userRoutes);
