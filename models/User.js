3199b55cf0f5c22d325021c891415023a91df461
Viveka31 <viveka310820@gmail.com>
2026-03-11 19:29:05 +0530

    fix mongoose pre save middleware

--- models/User.js
+++ models/User.js
@@ -24,18 +24,19 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-// Hash password before saving
-userSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+
+    if (!this.isModified("password")) return;
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
+
 });
 
-// Method to match entered password with hashed password
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
-}
+};
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
